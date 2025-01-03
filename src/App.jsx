c1b2f3c6ab04f6cbbd0bb8043363b52ce0c3c8e7
BatoolGhanima <batool00gh@gmail.com>
2025-01-03 08:47:13 +0300

    edit app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import axios from "axios";
-import {ProductShow} from "./ProductShow";
+import { ProductShow } from "./ProductShow";
+import React, { useEffect, useState } from 'react'
 function App() {
 
+  const [product, setProduct] = useState([])
+  const [error, setError] = useState("")
 
+  useEffect(() => {
+    const getData = async () => {
+      await axios
+        .get("https://dummyjson.com/products")
+        .then((res) => setProduct(res.data.products))
+        .catch((err) => setError(err.message))
+
+    }
+    getData();
+  }, [])
   return (
     <>
-      
-    <ProductShow title={"welcome in our gallery"}> </ProductShow>
+
+      <ProductShow title={"welcome in our gallery"} description={" we display a product and a discription of it, in addition to the category it belongs to and its price in dollar"} product={product} error={error}> </ProductShow>
 
     </>
   )
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 
-const Product = ({images,title,category,description,price}) => {
-    return (
-      <div className=' flex flex-col items-center w-[500px] '> 
-      <div className=' flesx flex-row items-center m-4 p-6 border border-gray-200  border-[2px] rounded-lg shadow-lg hover:shadow-xl h-[800px] overflow-auto'> 
-          <img src={images} alt={title}  className='w-[300px] self-center '/>
-          <h1 className=' text-[30px] text-pink-700 font-bold m-3 text-center'>  {title}</h1>
-          <p className='text-gray-500 py-4 hover:text-gray-700'> {description}</p>
-                   <p className='text-[24px] text-pink-700 font-semibold  w-full text-center h-16 rounded p-2 mb-2'>✨{category}✨</p>
-  <p className='font-bold text-[34px]  text-center text-[#132745] '> {price} $ </p>
-           {/* <p className='bg-custom-gradient border-spacing-2  border-red-100 text-[30px] text-[#304463] font-semibold  w-full text-center h-16 rounded p-2 mt-6'>{category}</p> */}
-            </div>
-        </div>
+const Product = ({ images, title, category, description, price }) => {
+  return (
+    <div className=' flex flex-col items-center w-[500px] '>
+      <div className=' flesx flex-row items-center m-4 p-6 border border-gray-200  border-[2px] rounded-lg shadow-lg hover:shadow-xl h-[750px] overflow-auto'>
+        <img src={images} alt={title} className='w-[300px] self-center ' />
+        <h1 className=' text-[30px] text-pink-700 font-bold m-3 text-center'>  {title}</h1>
+        <p className='text-gray-500 py-4 hover:text-gray-700'> {description}</p>
+        <p className='text-[24px] text-pink-700 font-semibold  w-full text-center h-16 rounded p-2 mb-2'>✨{category}✨</p>
+        <p className='font-bold text-[34px]  text-center text-[#132745] '> {price} $ </p>
+      </div>
+    </div>
   )
 }
 export default Product;
